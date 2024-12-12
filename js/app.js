e276f179c94ef038e502c4dc2eed19991b5c2c19
Gabytto <gabyttox@gmail.com>
2024-12-11 22:35:20 -0300

    avances en carrito, funciones cuentas

--- js/app.js
+++ js/app.js
@@ -6,4 +6,33 @@ function subtotal() {
     // console.log(suma)
     document.getElementById('modal-subtotal').innerText = suma  
 }
+function descuento() {
+    let a = parseInt(document.getElementById('modal-subtotal').textContent)
+    // console.log(a)
+    a = a * 0.1
+    // console.log(a)
+    document.getElementById('modal-descuento').innerHTML = a
+}
+
+function iva(){ 
+    let iva = parseInt(document.getElementById('modal-subtotal').textContent)
+    iva = iva * 0.21
+    document.getElementById('modal-iva').innerHTML = iva
+    // console.log(iva)
+}
+
+function totalAPagar(){
+    let a = parseInt(document.getElementById('modal-subtotal').textContent)
+    let b = parseInt(document.getElementById('modal-descuento').textContent)
+    let c = parseInt(document.getElementById('modal-iva').textContent)
+
+    let total = a - b + c
+    document.getElementById('modal-total').innerHTML = total
+
+}
+
+
 subtotal()
+descuento()
+iva()
+totalAPagar()
