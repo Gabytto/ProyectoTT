f5b34dfb12f2d796d7480c808a5d8adef28094f7
Gabytto <gabyttox@gmail.com>
2024-12-11 23:30:41 -0300

    carrito neto gravado y otros detalles

--- js/app.js
+++ js/app.js
@@ -3,30 +3,33 @@ function subtotal() {
     let b = parseInt(document.getElementById('precio-moto#2').textContent)
     let c = parseInt(document.getElementById('precio-moto#3').textContent)
     let suma = a+b+c
-    // console.log(suma)
     document.getElementById('modal-subtotal').innerText = suma  
 }
+
 function descuento() {
     let a = parseInt(document.getElementById('modal-subtotal').textContent)
-    // console.log(a)
     a = a * 0.1
-    // console.log(a)
-    document.getElementById('modal-descuento').innerHTML = a
+    document.getElementById('modal-descuento').innerHTML = '-' + a
 }
 
+function ng() {
+    let a = parseInt(document.getElementById('modal-subtotal').textContent)
+    let b = parseInt(document.getElementById('modal-descuento').textContent)
+    let suma = a+b
+    document.getElementById('modal-ng').innerText = suma  
+}
 function iva(){ 
-    let iva = parseInt(document.getElementById('modal-subtotal').textContent)
+    let iva = parseInt(document.getElementById('modal-ng').textContent)
     iva = iva * 0.21
     document.getElementById('modal-iva').innerHTML = iva
-    // console.log(iva)
+
 }
 
 function totalAPagar(){
     let a = parseInt(document.getElementById('modal-subtotal').textContent)
     let b = parseInt(document.getElementById('modal-descuento').textContent)
     let c = parseInt(document.getElementById('modal-iva').textContent)
-
-    let total = a - b + c
+    let total = a + b + c
     document.getElementById('modal-total').innerHTML = total
 
 }
@@ -34,5 +37,6 @@ function totalAPagar(){
 
 subtotal()
 descuento()
+ng()
 iva()
 totalAPagar()
